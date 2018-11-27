7a4e2080c649831f371daa60323bc7aaa298bbd1
lana <sitdownrightnow2552@gmail.com>
2018-11-27 13:09:09 +0700

    this shit worked

--- src/main/java/lana/application/controller/chatController/ChatSocketController.java
+++ src/main/java/lana/application/controller/chatController/ChatSocketController.java
@@ -9,6 +9,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+
 @Controller
 public class ChatSocketController {
     private MessageService messageService;
--- src/main/java/lana/application/support/configuration/FormatterAndConverterConfig.java
+++ src/main/java/lana/application/support/configuration/FormatterAndConverterConfig.java
@@ -1,20 +1,22 @@
-package lana.application.support.dataBinding.formatter;
+package lana.application.support.configuration;
 
 
 import lana.application.service.GroupService;
 import lana.application.service.UserService;
+import lana.application.support.dataBinding.formatter.GroupFormatter;
+import lana.application.support.dataBinding.formatter.UserFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class FormatterConfig implements WebMvcConfigurer {
+public class FormatterAndConverterConfig implements WebMvcConfigurer {
     private GroupService groupService;
     private UserService userService;
 
     @Autowired
-    public FormatterConfig(GroupService groupService, UserService userService) {
+    public FormatterAndConverterConfig(GroupService groupService, UserService userService) {
         this.groupService = groupService;
         this.userService = userService;
     }
